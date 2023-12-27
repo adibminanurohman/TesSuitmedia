07de3206608ff709ef9568753d238e58f0b92bb8
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2023-12-28 06:47:58 +0700

    Initial commit

---
+++ app/src/main/java/com/tessuitmedia/ThirdScreen.kt
@@ -0,0 +1,11 @@
+package com.tessuitmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_screen)
+    }
+}
