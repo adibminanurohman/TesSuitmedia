9d35ffa84823c373e2fbcf84b95aeaf271a2b388
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2023-12-28 08:45:34 +0700

    Feat : first screen page

--- app/src/main/java/com/tessuitmedia/FirstScreen.kt
+++ app/src/main/java/com/tessuitmedia/FirstScreen.kt
@@ -2,10 +2,40 @@ package com.tessuitmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class FirstScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_screen)
+
+        val inputName = findViewById<EditText>(R.id.edt_name)
+        val inputSentence = findViewById<EditText>(R.id.edt_palindrome)
+        val checkButton = findViewById<Button>(R.id.btn_check)
+
+        checkButton.setOnClickListener {
+            val palindrome = inputSentence.text.toString().replace("\\s".toRegex(), "")
+            val reversedPalindrome = palindrome.reversed()
+
+            val isSentence = palindrome.equals(reversedPalindrome, ignoreCase = true)
+
+            val message = if (isSentence){
+                "isPalindrome"
+            }else{
+                "not palindrome"
+            }
+
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Palindrome Check Result")
+            alertDialogBuilder.setMessage(message)
+            alertDialogBuilder.setPositiveButton("OK"){ dialog, _ ->
+                dialog.dismiss()
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
     }
 }
